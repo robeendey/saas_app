a5687d979a5b115ce2b56612d53dd4d112ac3061
PJoshi <pj@askpj.net>
2015-03-14 05:09:50 +0000

    Corrected misspelt t.sring to t.string in create_contacts -2

--- db/migrate/20150313232247_create_contacts.rb
+++ db/migrate/20150313232247_create_contacts.rb
@@ -2,7 +2,7 @@ class CreateContacts < ActiveRecord::Migration
   def change
     create_table :contacts do |t|
       t.string :name
-      t.sring :email
+      t.string :email
       t.text :comments
       
       t.timestamps
