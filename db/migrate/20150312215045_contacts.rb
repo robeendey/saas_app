c64865cf1a89a5b70d1e79d6dff263db78e08495
robeendey <robeendey@gmail.com>
2015-03-13 21:47:20 -0400

    Update 20150312215045_contacts.rb
    The colons were misplaced and you were missing another 'end' at the bottom.

--- db/migrate/20150312215045_contacts.rb
+++ db/migrate/20150312215045_contacts.rb
@@ -1,9 +1,10 @@
 class Contacts < ActiveRecord::Migration
   def change
     create_table :contacts do |t|
-      t.string: name
-      t.string: email
-      t.text: comments
+      t.string :name
+      t.string :email
+      t.text :comments
       t.timestamps
+    end
   end
 end
